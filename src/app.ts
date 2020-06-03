b7558540f4f880041a394503b6451edc644f4227
LeoChaeeeun <cheun0327@gmail.com>
2020-06-03 23:53:22 +0900

    express with graphQL

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,17 @@ import { GraphQLServer } from "graphql-yoga";
 import helmet from "helmet";
 import logger from "morgan";
 
-class App {
+class App {  
   public app: GraphQLServer;
-  constructor() {
+  constructor() {  //app class를 새로 시작할 때마다 호출되는 함수
     this.app = new GraphQLServer({});
     this.middlewares();
   }
   private middlewares = (): void => {
-    this.app.express.use(cors());
-    this.app.express.use(logger("dev"));
+    this.app.express.use(cors()); //graphQL은 express를 포함하고 있다.
+    this.app.express.use(logger("dev")); //logger의 type를 dev로 선언
     this.app.express.use(helmet());
   };
 }
 
 export default new App().app;
- 968  yarn.lock 
